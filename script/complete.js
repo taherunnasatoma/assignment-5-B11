4831568f978dc2c9a50154c9875fb2c984541bcc
TaherunNasaToma <taheruntoma@gmail.com>
2025-03-02 03:14:28 +0600

    added all the complete buttons

---
+++ script/complete.js
@@ -0,0 +1,44 @@
+
+
+const completeButtons = document.querySelectorAll('.btn-complt');
+        const devNum = document.getElementById('dev-number');
+        const taskNum=document.getElementById('task-num');
+        const container=document.getElementById('activity-container');
+        let completedTasks = 0; 
+
+       
+        for (let i = 0; i < completeButtons.length; i++) {
+          completeButtons[i].addEventListener('click', function () {
+                
+                alert('Board Updated Successfully');
+
+                const convertedDevnum = parseInt(devNum.innerText);
+                const sum= convertedDevnum + 1;
+                devNum.innerText = sum;
+
+                const convertedTasknum=parseInt(taskNum.innerText);
+                const substract=convertedTasknum-1;
+                taskNum.innerText=substract;
+
+
+
+                const div=document.createElement("div");
+                div.innerHTML=`
+                <p class="bg-slate-100 rounded-xl p-2">You have Complete The Task Add Dark Mode at 12:48:15 PM</p>
+                
+                `
+                container.appendChild(div)
+
+                
+                this.classList.add('bg-gray-400', 'cursor-not-allowed');
+                this.disabled = true;
+
+               
+                completedTasks++;
+
+                
+                if (completedTasks === completeButtons.length) {
+                    alert('Congrats!!!You have completed all the tasks');
+                }
+            });
+          }
